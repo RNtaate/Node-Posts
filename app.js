bd80689b7745d84d2860d9ba9d7f4c3c7e7e6a14
RNtaate <royhyde14@gmail.com>
2022-04-25 15:07:18 +0300

    Implement receiving of the post using a post request and adding them to the database

--- app.js
+++ app.js
@@ -24,15 +24,26 @@ db.on('error', (err) => {
 app.set("view engine", 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+app.use(express.urlencoded({extended: true}))
 app.use(morgan('dev'));
 
 
 app.get('/', async (req, res) => {
   try{
-    let posts = await PostModel.find();
+    let posts = await PostModel.find().sort({createdAt: -1});
     res.render('home', {posts});
   }catch(e) {
     console.error("Something is wrong", e);
+  }
+})
+
+app.post('/posts', async(req, res) => {
+  try{
+    let newPost = new PostModel(req.body);
+    await newPost.save();
+    res.redirect('/');
+  }catch(e) {
+    console.error(e);
   }
 })
 
