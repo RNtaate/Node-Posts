fe3b0b19e6038766151755fcb7902103f00c0b43
RNtaate <royhyde14@gmail.com>
2022-04-25 13:17:11 +0300

    Create database-config.js file
    Initialise connection to database using mongoose

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const mongoose = require('mongoose');
+
+const dbConfig = require('./config/database-config');
 
 const app = express();
+
+mongoose.connect(dbConfig.database);
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log("You have connected to mongoDB");
+})
+
+db.on('error', (err) => {
+  console.log("Connection Error: ", err);
+})
 
 app.set("view engine", 'ejs');
 app.set('views', path.join(__dirname, '/views'));
