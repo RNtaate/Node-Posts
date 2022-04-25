8794bbae1cb51702b3621a914a8fee359b8ad4f0
RNtaate <royhyde14@gmail.com>
2022-04-25 12:38:58 +0300

    Install morgan dependency using npm
    Use morgan in app.js file by making a request for home.html page

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('dev'));
+
+
+app.get('/', (req, res) => {
+  res.sendFile("./views/home.html", {root: __dirname});
+})
 
 app.listen(3000);
