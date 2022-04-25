d59ee57afa7419b3f38297e74ba3102d6cb37034
RNtaate <royhyde14@gmail.com>
2022-04-25 12:45:06 +0300

    Set th e app to use ejs as a view engine
    Change home.html to home.ejs

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
+
+app.set("view engine", 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(morgan('dev'));
 
 
 app.get('/', (req, res) => {
-  res.sendFile("./views/home.html", {root: __dirname});
+  res.render('home');
 })
 
 app.listen(3000);
