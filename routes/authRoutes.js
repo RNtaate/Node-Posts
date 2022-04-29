3185e18d68a678599d2087592c887e12fe9bc3b4
RNtaate <royhyde14@gmail.com>
2022-04-29 03:55:14 +0300

    Add routing for signing up a user, Use express flash

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const flash = require('express-flash');
 
 const router = express.Router();
+
+const User = require("../models/UserModel");
 
 router.get("/login", (req, res) => {
   res.render("auth/login");
@@ -8,6 +11,26 @@ router.get("/login", (req, res) => {
 
 router.get("/signup", (req, res) => {
   res.render("auth/signup");
+})
+
+router.post("/signup", async (req, res) => {
+  try {
+    let user = await User.findOne({email: req.body.email});
+    if (user) {
+      req.flash("error", "User with that email already exists");
+      res.render("auth/signup");
+    }else {
+      User.register(req.body, req.body.password, (err) => {
+        if(err) {
+          throw new Error();
+        }
+        req.flash("success", "You have successfully signed up!!")
+        res.redirect("/login");
+      })
+    }
+  }catch(e) {
+    console.error(e);
+  }
 })
 
 module.exports = router;
